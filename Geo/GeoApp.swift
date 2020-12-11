f10d74db493fa712d176d77cd2f553a1cf050fc3
cryptosarai <ok>
2020-12-11 08:53:25 +0700

    Initial Commit

---
+++ Geo/GeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoApp.swift
+//  Geo
+//
+//  Created by user on 11.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct GeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
